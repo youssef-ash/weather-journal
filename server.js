18cbba5661d661040e573fa98c45fe50fb306547
Youssef Ahmed <youssefashwal@outlook.com>
2023-01-16 04:04:13 +0200

    minor changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 projectData = {};
 
@@ -8,28 +8,28 @@ const app = express();
 
 app.use(express.static('website'));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // GET route.
 app.get('/all', getData);
 
 function getData(req, res) {
-    res.send(projectData);
-};
+  res.send(projectData);
+}
 
 // POST route.
 app.post('/addData', addData);
 
 function addData(req, res) {
-    projectData = {
-        date: req.body.date,
-        location: req.body.location,
-        temperature: req.body.temperature,
-        feelings: req.body.feelings
-    }
-    res.send(projectData);
-};
+  projectData = {
+    date: req.body.date,
+    location: req.body.location,
+    temperature: req.body.temperature,
+    feelings: req.body.feelings,
+  };
+  res.send(projectData);
+}
 
 const port = 3000;
 
